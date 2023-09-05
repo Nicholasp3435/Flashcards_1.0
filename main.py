22f30c9e1952eafbf807d461a18daa674f509f1a
Nicholasp3435 <nap44794@uga.edu>
2023-09-04 23:00:12 -0400

    feat: Add the ability for multiple correct answers

--- main.py
+++ main.py
@@ -2,14 +2,13 @@ import csv
 import random
 
 # Get the user to pick the set
-set = input('What set would you like to study? ')
-    
+set_name = input('What set would you like to study? ')
 
-# Defines the path based on the set name
-set_path = './' + set + '/'
+# Define the path based on the set name
+set_path = './' + set_name + '/'
 
-# Defines the csv file path
-csv_file = set_path + set + '.csv'
+# Define the csv file path
+csv_file = set_path + set_name + '.csv'
 
 # Initialize a dictionary to store flashcards
 flashcards = {}
@@ -19,10 +18,11 @@ try:
     with open(csv_file, mode='r', encoding='utf-8') as file:
         reader = csv.reader(file)
         for row in reader:
-            if len(row) == 2:
-                word, definition = row
-                flashcards[word.strip()] = definition.strip()
-except:
+            if len(row) >= 2:
+                word = row[0].strip()
+                definitions = [definition.strip() for definition in row[1:]]
+                flashcards[word] = definitions
+except FileNotFoundError:
     print('File not found :(')
 
 # Initialize a list to store the flashcards for spaced repetition
@@ -32,17 +32,17 @@ flashcard_queue = list(flashcards.keys())
 while flashcard_queue:
     # Randomly select a flashcard
     random_flashcard = random.choice(flashcard_queue)
-    correct_answer = flashcards[random_flashcard]
+    correct_definitions = flashcards[random_flashcard]
 
     # Show the flashcard
     print(f"Word: {random_flashcard}")
     user_answer = input('Definition: ')
 
     # Check the user's answer
-    if user_answer.strip() == correct_answer:
+    if user_answer.strip() in correct_definitions:
         print("Correct!\n")
         flashcard_queue.remove(random_flashcard)
     else:
-        print(f"Wrong; the correct answer is {correct_answer}\n")
+        print(f"Wrong; the correct answer(s) are: {', '.join(correct_definitions)}\n")
 
 print("You have completed all the flashcards!")
